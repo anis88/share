c8399ea7ed156a4ee85132b09e372edb17cdbf4a
Johannes <git@johannes-fischer.de>
2013-04-18 22:10:56 +0200

    shoing error when the track became unavailable

--- themes/share/javascript/soundcloud.js
+++ themes/share/javascript/soundcloud.js
@@ -3,8 +3,12 @@ var embedSoundcloud = function (client_id, sc_track_url) {
 		client_id: client_id
 	});
 	
-	var track_url = 'http://soundcloud.com/forss/flickermood';
-	SC.oEmbed(sc_track_url, { auto_play: false }, function(oEmbed) {
-		jQuery('#SoundcloudPlayer').html(oEmbed.html);
+	SC.oEmbed(sc_track_url, { auto_play: false, show_comments: false }, function(oEmbed) {
+		try {
+			jQuery('#SoundcloudPlayer').html(oEmbed.html);	
+		} catch(oembed) {
+			jQuery('#SoundcloudPlayer').html('<div class="alert-box alert"><p>Track not found: <a href="' + sc_track_url + '">' + sc_track_url + '</a></p></div>');
+		}
+		
 	});	
 };
